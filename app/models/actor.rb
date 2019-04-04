2246b9fbfad08b50e0cb49609dad7d361cd70953
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-04 18:34:55 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,16 +1,11 @@
 require "pry"
 
 class Actor < ActiveRecord::Base
-  attr_accessor :actor_id
   has_many :characters
   has_many :shows, through: :characters
 
   def full_name
     "#{self.first_name} #{self.last_name}"
-  end
-  
-  def list_roles
-    
   end
   
   def list_roles
